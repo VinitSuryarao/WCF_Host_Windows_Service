5d946a71796365dad67a2e593b87e024e5d5ffbe
Vinit Suryarao <suryarao.vinit@hotmail.com>
2022-09-25 17:41:04 +0530

    First Solution

---
+++ WindowsServiceHost/MyServiceHost.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using CalculatorLibrary;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace WindowsServiceHost
+{
+    public partial class MyServiceHost : ServiceBase
+    {
+        private ServiceHost host = null;
+        public MyServiceHost()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            host = new ServiceHost(typeof(CalculatorLibService));
+            host.Open();
+
+        }
+
+        protected override void OnStop()
+        {
+            if(host != null)
+            {
+                host.Close();
+            }
+            host = null;
+        }
+    }
+}
